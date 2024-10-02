608ce5b3deadd689fc48024fe52891cbe6d106d9
Nathan Nery <nbnery@gmail.com>
2024-10-01 21:42:10 -0300

    adj

--- projeto.py
+++ projeto.py
@@ -3,7 +3,7 @@ import pandas as pd
 from ydata_profiling import ProfileReport
 
 base = pd.read_csv("./BD/student_performance_prediction.csv")
-print(base.head())
+# print(base.head())
 
 base_limpa = base.dropna().copy()
 
@@ -23,9 +23,19 @@ base_limpa.loc[:, 'Passed'] = base_limpa['Passed'].map(conversao_yes_no)
 base_limpa.loc[:, 'Participation in Extracurricular Activities'] = base_limpa['Participation in Extracurricular Activities'].map(conversao_yes_no)
 base_limpa.loc[:, 'Parent Education Level'] = base_limpa['Parent Education Level'].map(conversao_educacao)
 
-print(base_limpa.head())
+base_limpa = base_limpa[base_limpa['Study Hours per Week'] >= 0]
+base_limpa = base_limpa[base_limpa['Attendance Rate'] <= 100]
+base_limpa = base_limpa[base_limpa['Previous Grades'] >= 0]
+base_limpa = base_limpa[base_limpa['Previous Grades'] <= 100]
+
+base_limpa = base_limpa.drop(columns=['Student ID'])
+
+# print(base_limpa.corr())
+correlacao = base_limpa.corr()
+
+correlacao.to_csv("correlacao.csv")
 
 
-analise = ProfileReport(base_limpa, title="Analise de Dados")
+# analise = ProfileReport(base_limpa, title="Analise de Dados")
 
-analise.to_file("Analise de Dados.html")
+# analise.to_file("Analise de Dados.html")
